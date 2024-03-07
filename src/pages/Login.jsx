d0234bd550eb4959574d4ecab4a9d3a10a9f0b9a
SHAKHZOD <113131580+shakhzod-madmarov@users.noreply.github.com>
2024-03-07 11:47:07 +0500

    some added pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,40 +1,63 @@
+import "../styles/pages/Login.css";
+
 const Login = () => {
   return (
-    <div className="container">
+    <div className="loginPage container">
       <section className="login">
         <h2>Войти</h2>
-        <form action="#" className="login__form">
-          <label className="login__form__label">Адрес электронной почты</label>
+        <form action="#" className="form">
+          <label className="form__label">Адрес электронной почты</label>
           <input
-            className="login__form__input"
+            className="form__input"
             type="email"
             placeholder="Введите e-mail"
           />
-          <label className="login__form__label">Пароль</label>
+          <label className="form__label">Пароль</label>
           <input
-            className="login__form__input"
+            className="form__input"
             type="password"
             placeholder="Введите пароль от аккаунт"
           />
-          <button type="submit" className="login__form__btn">
+          <button type="submit" className="form__btn">
             Войти
           </button>
         </form>
       </section>
       <section className="registration">
         <h2>Зарегистрироваться</h2>
-        <form action="#" className="registration__form">
-          <label>Фамилия</label>
-          <input type="text" placeholder="Введите свою фамилию" />
-          <label>Имя</label>
-          <input type="text" placeholder="Введите своё имя" />
-          <label>Адрес электронной почты</label>
-          <input type="email" placeholder="Введите e-mail" />
-          <label>Пароль</label>
-          <input type="password" placeholder="Введите пароль" />
-          <label>Повторите пароль</label>
-          <input type="password" placeholder="Повторите пароль" />
-          <button type="submit">Зарегистрироваться</button>
+        <form action="#" className="form">
+          <label className="form__label">Фамилия</label>
+          <input
+            type="text"
+            placeholder="Введите свою фамилию"
+            className="form__input"
+          />
+          <label className="form__label">Имя</label>
+          <input
+            type="text"
+            placeholder="Введите своё имя"
+            className="form__input"
+          />
+          <label className="form__label">Адрес электронной почты</label>
+          <input
+            type="email"
+            placeholder="Введите e-mail"
+            className="form__input"
+            className="form__input"
+          />
+          <label className="form__label">Пароль</label>
+          <input
+            type="password"
+            placeholder="Введите пароль"
+            className="form__input"
+          />
+          <label className="form__label">Повторите пароль</label>
+          <input
+            type="password"
+            placeholder="Повторите пароль"
+            className="form__input"
+          />
+          <button className="form__btn" type="submit">Зарегистрироваться</button>
         </form>
       </section>
     </div>
